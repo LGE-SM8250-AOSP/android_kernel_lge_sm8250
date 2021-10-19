2b2e3968baaf6e0bb8e0924e011e2534fa6d124b
阿菌•未霜 <799620521@qq.com>
2021-10-19 18:37:23 +0800

    mm: Use 3 threads per node for kswapd

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -141,7 +141,7 @@ struct scan_control {
 /*
  * Number of active kswapd threads
  */
-#define DEF_KSWAPD_THREADS_PER_NODE 1
+#define DEF_KSWAPD_THREADS_PER_NODE 3
 int kswapd_threads = DEF_KSWAPD_THREADS_PER_NODE;
 int kswapd_threads_current = DEF_KSWAPD_THREADS_PER_NODE;
 
